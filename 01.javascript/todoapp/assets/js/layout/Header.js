1b3da376e3b3733497fb23c53ca7ecef36d92fee
youngchae <010101chae@gmail.com>
2023-10-30 16:56:22 +0900

    [feat] 투두리스트 필터링 구현

--- 01.javascript/todoapp/assets/js/layout/Header.js
+++ 01.javascript/todoapp/assets/js/layout/Header.js
@@ -1,9 +1,36 @@
-const Header = function(title){
-  const headerNode = document.createElement('header');
-  const h1 = document.createElement('h1');
+const Header = function (title) {
+  const headerNode = document.createElement("header");
+  const h1 = document.createElement("h1");
   const headerTitle = document.createTextNode(title);
   h1.appendChild(headerTitle);
+
+  const filterBtns = document.createElement("div");
+  const allBtn = document.createElement("button");
+  allBtn.innerHTML = "All";
+  const activeBtn = document.createElement("button");
+  activeBtn.innerHTML = "Active";
+  const completedBtn = document.createElement("button");
+  completedBtn.innerHTML = "Completed";
+  filterBtns.appendChild(allBtn);
+  filterBtns.appendChild(activeBtn);
+  filterBtns.appendChild(completedBtn);
+
+  function filter(active, completed) {
+    const todoList = document.querySelectorAll("input[type=checkbox]");
+    todoList.forEach((todo) => {
+      if (!todo.checked) {
+        todo.parentNode.style.display = active ? "block" : "none";
+      } else {
+        todo.parentNode.style.display = completed ? "block" : "none";
+      }
+    });
+  }
+  allBtn.addEventListener("click", () => filter(true, true));
+  activeBtn.addEventListener("click", () => filter(true, false));
+  completedBtn.addEventListener("click", () => filter(false, true));
+
   headerNode.appendChild(h1);
+  headerNode.appendChild(filterBtns);
   return headerNode;
 };
 
