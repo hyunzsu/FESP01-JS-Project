718a2e4d566b42f2d8da0178656827991081f8fe
hyunzsu <kkm5169@naver.com>
2023-10-31 11:27:53 +0900

    [Refactor] TodoList2.js 함수 모듈화 리팩토링

--- 01.javascript/todoapp/assets/js/pages/list/TodoList2.js
+++ 01.javascript/todoapp/assets/js/pages/list/TodoList2.js
@@ -9,47 +9,58 @@ const TodoList2 = async () => {
   const infoArea = document.createElement('section');
   registBtn.appendChild(registTitle);
 
+  // 등록버튼 클릭 이벤트 처리
   registBtn.addEventListener('click', () => {
     const todoRegist = document.querySelector('.todo-regist');
-    if (todoRegist.style.display === 'none') {
-      return (todoRegist.style.display = 'block');
-    }
+    if (todoRegist.style.display === 'none') todoRegist.style.display = 'block';
   });
 
-  try {
-    const response = await axios('http://localhost:33088/api/todolist');
-    const { items } = response.data;
+  // 각 Todo 아이템을 생성하는 함수
+  const createTodoItem = (item) => {
+    const todoItem = document.createElement('li');
+    // 체크박스 생성
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = item.done;
+    checkbox.addEventListener('change', async (e) => {
+      // 체크박스 변경 시 서버에 업데이트 요청
+      try {
+        await axios.patch(`http://localhost:33088/api/todolist/${item._id}`, {
+          done: e.target.checked,
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    });
 
-    items.forEach((item) => {
-      const todoItem = document.createElement('li');
-      todoItem.addEventListener('click', async () => {
+    todoItem.addEventListener('click', async (e) => {
+      // 아이템 클릭 시 상세 정보 표시
+      if (e.target !== checkbox) {
         infoArea.innerHTML = '';
         infoArea.appendChild(await TodoInfo({ _id: item._id }));
-      });
-
-      // 체크박스
-      const checkbox = document.createElement('input');
-      checkbox.type = 'checkbox';
-      checkbox.checked = item.done;
-      checkbox.addEventListener('change', async (e) => {
-        try {
-          await axios.patch(`http://localhost:33088/api/todolist/${item._id}`, {
-            done: e.target.checked,
-          });
-        } catch (err) {
-          console.error(err);
-        }
-      });
-
-      todoItem.appendChild(checkbox);
-      todoList.appendChild(todoItem);
-
-      const title = document.createTextNode(item.title);
-      todoItem.appendChild(title);
+      }
     });
-  } catch (err) {
-    console.error(err);
-  }
+
+    todoItem.appendChild(checkbox);
+    todoList.appendChild(todoItem);
+
+    const title = document.createTextNode(item.title);
+    todoItem.appendChild(title);
+  };
+
+  // 데이터를 가져와 화면에 표시하는 함수
+  const initializeTodoList = async () => {
+    try {
+      const response = await axios('http://localhost:33088/api/todolist');
+      const { items } = response.data;
+      items.forEach((item) => createTodoItem(item));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // 페이지 초기화 함수 호출
+  initializeTodoList();
 
   page.appendChild(registBtn);
   page.appendChild(todoList);
@@ -58,5 +69,4 @@ const TodoList2 = async () => {
 
   return page;
 };
-
 export default TodoList2;
