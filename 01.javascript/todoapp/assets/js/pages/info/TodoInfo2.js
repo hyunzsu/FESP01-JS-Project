f5827415f6c843a540a1d6898472de961e28fed6
kimzeze <fridaynight@kakao.com>
2023-10-31 10:14:05 +0900

    [comment] Todoinfo에 필요한 주석 추가

--- 01.javascript/todoapp/assets/js/pages/info/TodoInfo2.js
+++ 01.javascript/todoapp/assets/js/pages/info/TodoInfo2.js
@@ -3,71 +3,70 @@ import Footer from "../../layout/Footer.js";
 
 const TodoInfo = async ({ _id }) => {
   try {
-    const response = await axios(`http://localhost:33088/api/todolist/${_id}`);
-    const item = response.data.item;
-    // console.log(response);
-
+    /* 상세내용 마크업 */
     const page = document.createElement("div");
-    page.setAttribute("id", "page");
-
     const title = document.createElement("input");
-    title.setAttribute("disabled", true);
     const content = document.createElement("input");
+    const createTime = document.createElement("div");
+
+    page.setAttribute("id", "page");
+    title.setAttribute("disabled", true);
     content.setAttribute("disabled", true);
 
-    const createTime = document.createElement("div");
+    /* [삭제] [수정] 버튼 마크업 */
     const buttonContainer = document.createElement("div");
-
     const deleteButton = document.createElement("button");
     const editButton = document.createElement("button");
+
+    deleteButton.textContent = "삭제";
+    editButton.textContent = "수정";
+
+    /* 상세 내용 받아오기  */
+    const response = await axios(`http://localhost:33088/api/todolist/${_id}`);
+    const item = response.data.item;
 
     title.value = `${item.title}`;
     content.value = `${item.content}`;
     createTime.textContent = `${item.createdAt}`;
-    deleteButton.textContent = "삭제";
-    editButton.textContent = "수정";
 
     /* 수정-삭제 기능 이벤트 함수 */
     editButton.addEventListener("click", async (e) => {
-      // 수정버튼을 누르면 [제목],[내용] input에 disabled 속성 삭제
+      /* A. [수정] 버튼을 누르면 [제목],[내용]의 input에서 disabled 속성 삭제 */
       if (editButton.textContent == "수정") {
-        /* 1. input의 disabled 속성 삭제 */
-        console.log("수정 버튼 클릭 이벤트 발생");
         title.removeAttribute("disabled");
         content.removeAttribute("disabled");
 
-        /* 2. [수정] -> [완료]로 변경, [삭제] -> [취소]로 글자 수정 */
+        /* A-1. [수정] -> [완료]로 변경, [삭제] -> [취소]로 글자 수정 */
         editButton.textContent = "완료";
         deleteButton.textContent = "취소";
       } else {
-        /* 1. 완료 버튼을 누르면 [제목],[내용] input에 disabled 속성 추가 */
+        /* B. [완료] 버튼을 누르면 [제목],[내용] input에 disabled 속성 추가 */
         title.setAttribute("disabled", true);
         content.setAttribute("disabled", true);
 
-        /* 1. [완료] -> [수정]로 변경, [취소] -> [삭제]로 글자 수정 */
+        /* B=1. [완료] -> [수정]로 변경, [취소] -> [삭제]로 글자 수정 */
         editButton.textContent = "수정";
         deleteButton.textContent = "삭제";
 
-        /* editData에 title, content 값 전달 */
-        let editData = {
+        /* C. 수정한 내용 서버로 전송 */
+        const updatedData = {
           title: `${title.value}`,
           content: `${content.value}`,
         };
-        console.log(editData);
-        editTodo(_id, editData);
+        editTodo(_id, updatedData);
       }
     });
 
-    /* 수정한 내용 axios.patch를 통해 전송하는 함수 */
-    const editTodo = async (_id, updateData) => {
+    /* 수정 내용 전송하는 함수 */
+    const editTodo = async (_id, updatedData) => {
       try {
         const response = await axios.patch(
           `http://localhost:33088/api/todolist/${_id}`,
-          updateData
+          updatedData
         );
         console.log("수정해서 전송한 내역 -> ", response.data);
       } catch (error) {
-        console.error("수정 에러", error);
+        console.error("수정내역 전송 에러", error);
       }
     };
 
