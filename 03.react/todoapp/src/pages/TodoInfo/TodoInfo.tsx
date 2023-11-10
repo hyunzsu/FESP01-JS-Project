f12d21e80e4f65f2f5ac834279da8f6c39b8b0c7
jungmyungjin <myung2667@gmail.com>
2023-11-10 16:17:11 +0900

    [Refactor] TodoInfo React 마이그레이션

--- 03.react/todoapp/src/pages/TodoInfo/TodoInfo.tsx
+++ 03.react/todoapp/src/pages/TodoInfo/TodoInfo.tsx
@@ -1,17 +1,109 @@
-const TodoInfo = () => {
+import { useState, useEffect } from "react";
+import axios from "axios";
+import TodoDelete from "../../utils/TodoDelete";
 
+interface TodoInfoProps {
+  id: number;
+}
 
-  return <section>
-    <div className="info-container">
-        <input type="text" placeholder="TODO 제목을 입력하세요" className="title-input" disabled={true} maxLength={25} />
+const TodoInfo = (props: TodoInfoProps) => {
+  const id = props.id;
+  const [isUpdate, setIsUpdate] = useState(false);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [isDone, setIsDone] = useState(false);
+
+  let item: TodoItem;
+
+  const getTodoDetail = async () => {
+    try {
+      const response: TodoResponse = await axios.get(
+        `http://localhost:33088/api/todolist/${id}`
+      );
+      if (response.data.ok === 1) {
+        item = response.data.item;
+        console.log("item=", item);
+        setTitle(item.title);
+        setContent(item.content);
+        setIsDone(item.done);
+      }
+      // console.log(response);
+      console.log("api 불러오기.");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getTodoDetail();
+  }, []);
+
+  const handleUpdateButton = async () => {
+    setIsUpdate(!isUpdate);
+    if (isUpdate === true) {
+      const response: TodoResponse = await axios.patch(
+        `http://localhost:33088/api/todolist/${id}`,
+        {
+          title: title,
+          content: content,
+          done: isDone,
+        }
+      );
+      console.log("업데이트.");
+      if (response.data.ok === 1) {
+        item = response.data.item;
+        console.log("업데이트 된 내용!!!!=", item);
+      }
+    }
+  };
+
+  const onChangeTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target instanceof HTMLInputElement) {
+      setTitle(event.target.value);
+    }
+  };
+  const onChangeContent = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    if (event.target instanceof HTMLTextAreaElement)
+      setContent(event.target.value);
+  };
+
+  return (
+    <section>
+      <div className="info-container">
+        <input
+          type="text"
+          placeholder="TODO 제목을 입력하세요"
+          className="title-input"
+          disabled={isUpdate === true ? false : true}
+          maxLength={25}
+          value={title}
+          onChange={onChangeTitle}
+        />
         <div className="time"></div>
-        <textarea className="content-textarea" placeholder="TODO 상세 내용을 입력하세요" disabled={true}></textarea>
-      <div className="button-container">
-        <button className="info-button edit">수정</button>
-        <button className="info-button delete">삭제</button>
+        <textarea
+          className="content-textarea"
+          placeholder="TODO 상세 내용을 입력하세요"
+          disabled={isUpdate === true ? false : true}
+          value={content}
+          onChange={onChangeContent}
+        ></textarea>
+        <div className="button-container">
+          <button onClick={handleUpdateButton} className="info-button edit">
+            {isUpdate === false ? "수정" : "완료"}
+          </button>
+          <button
+            onClick={() => {
+              TodoDelete(id);
+            }}
+            className="info-button delete"
+          >
+            삭제
+          </button>
+        </div>
       </div>
-    </div>;
-  </section>
+      ;
+    </section>
+  );
 };
 
 export default TodoInfo;
