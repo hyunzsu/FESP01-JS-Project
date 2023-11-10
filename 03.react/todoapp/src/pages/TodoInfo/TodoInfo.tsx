57ec8b3e127ea11528627d2bac8e5123a39e41ae
hyeonjuuu <janghyeonjuu@gmail.com>
2023-11-10 14:29:52 +0900

    TodoInfo.tsx 마크업 완료

--- 03.react/todoapp/src/pages/TodoInfo/TodoInfo.tsx
+++ 03.react/todoapp/src/pages/TodoInfo/TodoInfo.tsx
@@ -1,5 +1,17 @@
 const TodoInfo = () => {
-  return <div>TodoInfo</div>;
+
+
+  return <section>
+    <div className="info-container">
+        <input type="text" placeholder="TODO 제목을 입력하세요" className="title-input" disabled={true} maxLength={25} />
+        <div className="time"></div>
+        <textarea className="content-textarea" placeholder="TODO 상세 내용을 입력하세요" disabled={true}></textarea>
+      <div className="button-container">
+        <button className="info-button edit">수정</button>
+        <button className="info-button delete">삭제</button>
+      </div>
+    </div>;
+  </section>
 };
 
 export default TodoInfo;
