9f457bee74ea4f283254b16d687dd5185fc7b572
김진우 <rlawlsdn263@naver.com>
2023-11-16 14:46:15 +0900

    [Feat] 검색기능 추가
    사용자가 input에 값을 입력하면 onChange 이벤트가 동작하면서 handleSearch 핸들러가 실행됩니다.
    
    handleSearch 핸들러는 GET 통신을 하여 서버로부터 데이터를 가져와 사용자가 입력한 input값과 일치한 값을 반환합니다.
    
    현재는 input이 입력될 때마다 GET 통신이 이뤄지고 있기 때문에 데이터의 용량이 클 경우에는 성능 저하가 발생할 수 있어 향후
    Debounce 기능을 추가할 예정입니다.

--- 03.react/todoapp/src/components/SearchBar/SearchBar.tsx
+++ 03.react/todoapp/src/components/SearchBar/SearchBar.tsx
@@ -1,6 +1,20 @@
+import axios from 'axios';
+
 const SearchBar = () => {
+
+    const handleSearch = async (e) => {
+        const target = e.target.value;
+
+        const response = await axios(`http://localhost:33088/api/todolist`);
+        const data = response.data.items;
+        
+        // 인풋에 입력된 값과 데이터를 대조해 일치하는 값을 반환한다
+        const searchResult = data.filter(item => item.title === target);
+        return searchResult;
+    }
+
     return <div className="SearchBar">
-        <input type="text" placeholder='검색어를 입력해주세요' />
+        <input type="text" placeholder='검색어를 입력해주세요' onBlur={handleSearch}/>
     </div>
 }
 
