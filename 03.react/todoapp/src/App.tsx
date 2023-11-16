f6bf57149b6ae19f78053629f8bed17a7525cd0c
hyunzsu <kkm5169@naver.com>
2023-11-16 14:12:36 +0900

    [Feat] 다크모드 구현

--- 03.react/todoapp/src/App.tsx
+++ 03.react/todoapp/src/App.tsx
@@ -1,15 +1,18 @@
-import { useState } from "react";
-import { TodoList } from "@/pages/_index";
-import { Header, Footer } from "@/layout/_index";
+import { useState } from 'react';
+import { TodoList } from '@/pages/_index';
+import { Header, Footer } from '@/layout/_index';
+import { DarkModeProvider } from './context/DarkModeContext';
 
 function App() {
   const [todoItem, setTodoItem] = useState([]);
 
   return (
-    <div id="app">
-      <Header setTodoItem={setTodoItem} />
-      <TodoList todoItem={todoItem} setTodoItem={setTodoItem} />
-      <Footer />
+    <div id='app'>
+      <DarkModeProvider>
+        <Header setTodoItem={setTodoItem} />
+        <TodoList todoItem={todoItem} setTodoItem={setTodoItem} />
+        <Footer />
+      </DarkModeProvider>
     </div>
   );
 }
