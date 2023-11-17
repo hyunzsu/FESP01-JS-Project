b8f414f6c38c464daae5893f1fa88443c4c3431a
kimzeze <fridaynight@kakao.com>
2023-11-17 15:16:23 +0900

    [💄 style]: TodoRegist TailwindCSS 적용
    TodoRegist.tsx에서 기존에 사용되던 CSS를 제거하고 tailwind css를 적용했다.

--- 03.react/todoapp/src/pages/TodoRegist/TodoRegist.tsx
+++ 03.react/todoapp/src/pages/TodoRegist/TodoRegist.tsx
@@ -54,13 +54,13 @@ const TodoRegist = ({ showRegist, setShowRegist }) => {
     <section>
       <div
         style={{ display: showRegist ? "block" : "none" }}
-        className="regist-container"
+        className="w-[480px] h-[500px] p-[30px] ml-[20px] flex flex-col rounded-[10px] bg-sub"
       >
         <form action="">
-          <div className="button-container">
+          <div className="w-[210px] h-[40px] flex justify-between items-center mt-0 mr-0 mb-[10px] ml-[5px]">
             <button
               onClick={handleRegistButton}
-              className="regist-buttons save"
+              className="w-[50px] h-[30px] bg-add text-[18px] font-semibold rounded-[5px] shadow-none cursor-pointer text-main border border-main"
             >
               저장
             </button>
@@ -69,7 +69,7 @@ const TodoRegist = ({ showRegist, setShowRegist }) => {
                 e.preventDefault();
                 setShowRegist(false);
               }}
-              className="regist-buttons cancel"
+              className="w-[50px] h-[30px] bg-add text-[18px] font-semibold rounded-[5px] shadow-none cursor-pointer text-cancel border border-cancel"
             >
               취소
             </button>
@@ -78,12 +78,12 @@ const TodoRegist = ({ showRegist, setShowRegist }) => {
                 e.preventDefault();
                 handleImageButton();
               }}
-              className="regist-buttons image"
+              className="w-[90px] h-[30px] bg-add text-[18px] font-semibold rounded-[5px] shadow-none cursor-pointer text-main border border-main"
             >
               image
             </button>
             <input
-              className="image-input"
+              className="hidden"
               type="file"
               accept="image/*"
               ref={imageInput}
@@ -98,13 +98,13 @@ const TodoRegist = ({ showRegist, setShowRegist }) => {
           <input
             type="text"
             placeholder="TODO 제목을 입력하세요"
-            className="regist-title"
+            className="text-[24px] h-[30px] font-bold mb-[30px] order-1 w-[420px] pl-[5px] rounded-[5px]"
             maxLength={25}
             value={title}
             onChange={onChangeTitle}
           />
           <textarea
-            className="regist-content"
+            className="text-[20px] h-[300px] font-normal pt-[10px] order-2 w-[420px] pl-[5px] rounded-[5px]"
             placeholder="TODO 상세 내용을 입력하세요"
             value={content}
             onChange={onChangeContent}
